1a05b2b0cc8e9e4c32bffe285da91831344d8357
LChoquet <57703110+LChoquet@users.noreply.github.com>
2021-10-15 11:01:42 +0200

    Récupération psnStatut depuis l'API (#1)
    * Récupération psnStatut depuis l'API
    
    * Refacto pour généraliser la méthode de requête
    
    * Refacto généralisation supplémentaire
    
    * Ajout d'un espace pour lisibilité logs
    
    * Fix erreur lecture JSON sur mapper
    
    Co-authored-by: LChoquet <lucas.choquet@digiwin.fr>

---
+++ WEB-INF/classes/fr/cg44/plugin/inforoutes/api/InforoutesApiRequestManager.java
@@ -0,0 +1,59 @@
+package fr.cg44.plugin.inforoutes.api;
+
+import org.apache.log4j.Logger;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jalios.jcms.Channel;
+
+import fr.cg44.plugin.inforoutes.dto.PsnStatutDTO;
+import fr.cg44.plugin.socle.ApiUtil;
+
+public class InforoutesApiRequestManager {
+    
+    private static final Logger LOGGER = Logger.getLogger(InforoutesApiRequestManager.class);
+    
+    private static Channel channel = Channel.getChannel();
+    
+    private static String baseUrl = channel.getProperty("jcmsplugin.inforoutes.api.root");
+    
+    private static String suffixPsnStatut = channel.getProperty("jcmsplugin.inforoutes.api.psnstatus");
+    
+    /**
+     * Récupérer le JSON renvoyé par une URL sous la forme d'un InputStream, utilisé plus tard pour un ObjectMapper
+     * qui définiera un objet Java
+     */
+    private static String getDtoJsonStringFromApi(String url) {
+        
+      return ApiUtil.getJsonObjectFromApi(url, null).toString();
+      
+    }
+    
+    /**
+     * Renvoie un objet Java de la classe indiquée depuis un JSON récupéré depuis l'API Inforoutes
+     * @param <T>
+     * @param clazz
+     * @param url
+     * @return
+     */
+    private static <T> Object getObjectFromJson(Class<T> clazz, String url) {
+      ObjectMapper mapper = new ObjectMapper();
+      mapper.configure(DeserializationFeature.FAIL_ON_IGNORED_PROPERTIES, false);
+      try {
+        return mapper.readValue(getDtoJsonStringFromApi(url), clazz);
+      } catch (Exception e) {
+        LOGGER.warn("Erreur sur getObjectFromJson pour classe " + clazz.getName() + " -> " + e.getMessage());
+        return null;
+      }
+    }
+    
+    /**
+     * Renvoie un objet PsnStatut à partir du JSON fourni par l'API Inforoute
+     * En cas d'échec, l'object est 'null'
+     * @return
+     */
+    public static PsnStatutDTO getPsnStatut() {
+      return (PsnStatutDTO) getObjectFromJson(PsnStatutDTO.class, baseUrl + suffixPsnStatut);
+    }
+    
+}
